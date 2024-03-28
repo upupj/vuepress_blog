0d550d3b6140ba16148a95193ab1fa0c3eacd7f3
jiangyimin <yimin.jiang@wbstar.com>
2024-03-28 16:51:22 +0800

    1

--- docs/.vuepress/.temp/pages/index.html.js
+++ docs/.vuepress/.temp/pages/index.html.js
@@ -1,5 +1,5 @@
 import comp from "/Users/jiangyimin/Documents/personal/vuepress_blog/docs/.vuepress/.temp/pages/index.html.vue"
-const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"hostname\":\"https://www.jiangyimin.fun\",\"colorModeSwitch\":false,\"home\":true,\"heroImage\":\"https://vuejs.press/images/hero.png\",\"heroText\":\"Jim's Blog\",\"tagline\":\"欢迎来到我的博客\",\"footer\":\"MIT Licensed | Copyright © 2018-present VuePress Community\"},\"headers\":[],\"git\":{\"updatedTime\":null,\"contributors\":[]},\"filePathRelative\":\"index.md\"}")
+const data = JSON.parse("{\"path\":\"/\",\"title\":\"\",\"lang\":\"zh-CN\",\"frontmatter\":{\"hostname\":\"https://www.jiangyimin.fun\",\"colorModeSwitch\":false,\"home\":true,\"heroImage\":\"https://vuejs.press/images/hero.png\",\"heroText\":\"Jim's Blog\",\"tagline\":\"欢迎来到我的博客\",\"footer\":\"MIT Licensed | Copyright © 2018-present VuePress Community\"},\"headers\":[],\"git\":{\"updatedTime\":1711615853000,\"contributors\":[{\"name\":\"jiangyimin\",\"email\":\"yimin.jiang@wbstar.com\",\"commits\":1}]},\"filePathRelative\":\"index.md\"}")
 export { comp, data }
 
 if (import.meta.webpackHot) {
